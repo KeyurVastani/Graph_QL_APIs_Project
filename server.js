c5816e88d34be87ffe82e11d060f783b8df338db
Keyur_Vastani <keyur_vastani@Keyur-Vastanis-MacBook-Air.local>
2023-02-26 16:40:30 +0530

    create different get data operation using query

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { users} from './fakeData.js'
+import { users,quotes} from './fakeData.js'
 
 //create a schema
 const typeDefs = `#graphql
   type Query {
     greet:String
     users:[User]
+    singleUser(id:ID!):User
+    quotes:[Quote]
+    individualQuote(by:ID!):[Quote]
   }
 
   type User{
-    id:ID
+    id:ID!
     firstName:String
     lastName:String
     email:String
+    quotes:[Quote]
+  }
+  type Quote{
+    name:String
+    by:ID
+    createrName:String
   }
 `;
 
 const resolvers={
   Query:{
         users:()=>users,
-        greet:()=>"Keyur vastani"
+        greet:()=>"Keyur vastani",
+        quotes:()=>quotes,
+        singleUser:(_,args)=>users.find(user=>user.id===args.id),
+        individualQuote:(_,{by})=>quotes.filter(quote=>quote.by===by)
+  },
+  User:{
+    quotes:(parentUser)=>quotes.filter((quote)=>quote.by===parentUser.id)
+  },
+Quote:{
+  createrName:(quote)=> {
+    const {firstName}=users.find(user=>user.id===quote.by)
+    return firstName
   }
+},
 }
 
 const server=new ApolloServer({typeDefs,resolvers})

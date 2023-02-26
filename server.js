04f5a90fc5df2c0ba0eb5faa5e72e33c51e0a7c3
Keyur_Vastani <keyur_vastani@Keyur-Vastanis-MacBook-Air.local>
2023-02-26 10:36:41 +0530

    user query 2

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
-
+import { users} from './fakeData.js'
 
 //create a schema
 const typeDefs = `#graphql
   type Query {
-    greet: String
+    greet:String
+    users:[User]
+  }
+
+  type User{
+    id:ID
+    firstName:String
+    lastName:String
+    email:String
   }
 `;
 
 const resolvers={
   Query:{
-        greet:()=>"hello world"
+        users:()=>users,
+        greet:()=>"Keyur vastani"
   }
 }
 

d5d36b3ad2111b20a53e64d24ed662c424adb2fe
Keyur_Vastani <keyur_vastani@Keyur-Vastanis-MacBook-Air.local>
2023-02-27 21:47:50 +0530

    connected the mongodb database

---
+++ config.js
@@ -0,0 +1,2 @@
+export const MONGO_URI =
+  "mongodb+srv://Keyur1711:Keyur1711@cluster0.qy5si.mongodb.net/graphqldb";
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { startStandaloneServer } from "@apollo/server/standalone";
 import typeDefs from "./schema.js";
 import resolvers from "./resolvers.js";
+import mongoose from "mongoose";
+import { MONGO_URI } from "./config.js";
 
-const server=new ApolloServer({typeDefs,resolvers})
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongodb connected =================== success");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("coonection error================error", err);
+});
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
